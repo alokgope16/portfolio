ca096682e94d29623e9c713623d249978e97d17c
Alok Kumar Gope <gopealok.04@gmail.com>
2024-10-04 00:39:09 +0530

    New Files Added

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+var navbar = document.getElementById("navbar");
+var header = document.getElementById("header");
+function openmenu() {
+  navbar.style.right = "0";
+  header.style.right = "0";
+}
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
+
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -18,3 +42,20 @@ window.onscroll = () => {
 
   });
 };
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxm9E5MirI2uSLv7pJVYyQw86krpoC9xDhcWB8Kimmu-HClexe6hawpPbL8x-mYB4dhCw/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => {
+      msg.innerHTML = "Message sent successfully"
+      setTimeout(function () {
+        msg.innerHTML = ""
+      }, 5000)
+      form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
